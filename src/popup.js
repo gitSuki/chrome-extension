41061a53fe2dbe1bc6bb4fa78410246e642fdef9
gitSuki <collinbrown226@gmail.com>
2022-07-11 08:25:22 -0500

    Create storage functionality for user's search preferences

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,22 @@
 'use strict';
 const searchbar = document.getElementById('searchbar');
-const malButton = document.getElementById('myanimelist')
-const anilistButton = document.getElementById('anilist')
+const malButton = document.getElementById('myanimelist');
+const anilistButton = document.getElementById('anilist');
 const searchResultsContainer = document.getElementById('container');
 
+(function getWebsitePreference (){
+  chrome.storage.sync.get(['preference'], result => {
+    if (result.preference === malButton.id){
+      malButton.checked = true;
+    }
+    else {
+      anilistButton.checked = true;
+    }
+  });
+})()
+
 searchbar.addEventListener('keyup', async () => {
-  const animeID = searchbar.value
+  const animeID = searchbar.value;
   if (malButton.checked){
       searchResultsContainer.textContent = "MAL: " + animeID;
   }
@@ -14,112 +25,124 @@ searchbar.addEventListener('keyup', async () => {
   }
 })
 
+malButton.addEventListener('click', () => {
+  saveWebsitePreference(malButton.id);
+})
 
-(function () {
-  // We will make use of Storage API to get and store `count` value
-  // More information on Storage API can we found at
-  // https://developer.chrome.com/extensions/storage
+anilistButton.addEventListener('click', () => {
+  saveWebsitePreference(anilistButton.id);
+})
 
-  // To get storage access, we have to mention it in `permissions` property of manifest.json file
-  // More information on Permissions can we found at
-  // https://developer.chrome.com/extensions/declare_permissions
-  const counterStorage = {
-    get: (cb) => {
-      chrome.storage.sync.get(['count'], (result) => {
-        cb(result.count);
-      });
-    },
-    set: (value, cb) => {
-      chrome.storage.sync.set(
-        {
-          count: value,
-        },
-        () => {
-          cb();
-        }
-      );
-    },
-  };
+function saveWebsitePreference (websitePreference){
+  chrome.storage.sync.set({preference: websitePreference});
+}
 
-  function setupCounter(initialValue = 0) {
-    document.getElementById('counter').innerHTML = initialValue;
 
-    document.getElementById('incrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'INCREMENT',
-      });
-    });
+// (function () {
+//   // We will make use of Storage API to get and store `count` value
+//   // More information on Storage API can we found at
+//   // https://developer.chrome.com/extensions/storage
 
-    document.getElementById('decrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'DECREMENT',
-      });
-    });
-  }
+//   // To get storage access, we have to mention it in `permissions` property of manifest.json file
+//   // More information on Permissions can we found at
+//   // https://developer.chrome.com/extensions/declare_permissions
+//   const counterStorage = {
+//     get: (cb) => {
+//       chrome.storage.sync.get(['count'], (result) => {
+//         cb(result.count);
+//       });
+//     },
+//     set: (value, cb) => {
+//       chrome.storage.sync.set(
+//         {
+//           count: value,
+//         },
+//         () => {
+//           cb();
+//         }
+//       );
+//     },
+//   };
 
-  function updateCounter({ type }) {
-    counterStorage.get((count) => {
-      let newCount;
+//   function setupCounter(initialValue = 0) {
+//     document.getElementById('counter').innerHTML = initialValue;
 
-      if (type === 'INCREMENT') {
-        newCount = count + 1;
-      } else if (type === 'DECREMENT') {
-        newCount = count - 1;
-      } else {
-        newCount = count;
-      }
+//     document.getElementById('incrementBtn').addEventListener('click', () => {
+//       updateCounter({
+//         type: 'INCREMENT',
+//       });
+//     });
 
-      counterStorage.set(newCount, () => {
-        document.getElementById('counter').innerHTML = newCount;
+//     document.getElementById('decrementBtn').addEventListener('click', () => {
+//       updateCounter({
+//         type: 'DECREMENT',
+//       });
+//     });
+//   }
 
-        // Communicate with content script of
-        // active tab by sending a message
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
+//   function updateCounter({ type }) {
+//     counterStorage.get((count) => {
+//       let newCount;
 
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              type: 'COUNT',
-              payload: {
-                count: newCount,
-              },
-            },
-            (response) => {
-              console.log('Current count value passed to contentScript file');
-            }
-          );
-        });
-      });
-    });
-  }
+//       if (type === 'INCREMENT') {
+//         newCount = count + 1;
+//       } else if (type === 'DECREMENT') {
+//         newCount = count - 1;
+//       } else {
+//         newCount = count;
+//       }
 
-  function restoreCounter() {
-    // Restore count value
-    counterStorage.get((count) => {
-      if (typeof count === 'undefined') {
-        // Set counter value as 0
-        counterStorage.set(0, () => {
-          setupCounter(0);
-        });
-      } else {
-        setupCounter(count);
-      }
-    });
-  }
+//       counterStorage.set(newCount, () => {
+//         document.getElementById('counter').innerHTML = newCount;
 
-  document.addEventListener('DOMContentLoaded', restoreCounter);
+//         // Communicate with content script of
+//         // active tab by sending a message
+//         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//           const tab = tabs[0];
 
-  // Communicate with background file by sending a message
-  chrome.runtime.sendMessage(
-    {
-      type: 'GREETINGS',
-      payload: {
-        message: 'Hello, my name is Pop. I am from Popup.',
-      },
-    },
-    (response) => {
-      console.log(response.message);
-    }
-  );
-})();
+//           chrome.tabs.sendMessage(
+//             tab.id,
+//             {
+//               type: 'COUNT',
+//               payload: {
+//                 count: newCount,
+//               },
+//             },
+//             (response) => {
+//               console.log('Current count value passed to contentScript file');
+//             }
+//           );
+//         });
+//       });
+//     });
+//   }
+
+//   function restoreCounter() {
+//     // Restore count value
+//     counterStorage.get((count) => {
+//       if (typeof count === 'undefined') {
+//         // Set counter value as 0
+//         counterStorage.set(0, () => {
+//           setupCounter(0);
+//         });
+//       } else {
+//         setupCounter(count);
+//       }
+//     });
+//   }
+
+//   document.addEventListener('DOMContentLoaded', restoreCounter);
+
+//   // Communicate with background file by sending a message
+//   chrome.runtime.sendMessage(
+//     {
+//       type: 'GREETINGS',
+//       payload: {
+//         message: 'Hello, my name is Pop. I am from Popup.',
+//       },
+//     },
+//     (response) => {
+//       console.log(response.message);
+//     }
+//   );
+// })();
